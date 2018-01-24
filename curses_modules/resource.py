d1e202d49b103e367c752e617ca86e4c3cb5d1bb
沈伟 <shenwei0329@hotmail.com>
2018-01-24 14:56:11 +0800

    增加资源类
    增加资源类。

---
+++ curses_modules/resource.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+#
+#   资源类 Resource
+#   ===============
+#
+
+
+class Resource:
+
+    def __init__(self, MAX_X, MAX_Y):
+        pass
+
+    def get(self, x, y):
+        pass
+
+    def refresh(self):
+        pass
