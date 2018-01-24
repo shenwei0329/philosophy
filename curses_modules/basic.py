ee30b47e820a2edcc24338beb3195cba60a7470a
沈伟 <shenwei@chenweideMac-mini.local>
2018-01-24 22:50:40 +0800

    增加“窗口模式”。

--- curses_modules/basic.py
+++ curses_modules/basic.py
@@ -7,15 +7,73 @@
 import curses
 import json
 
+MENU_W = 40
+
+class TimeScale:
+
+    def __init__(self):
+        self.TS = 0
+        self.year = 0
+        self.month = 0
+        self.day = 0
+        self._load('timescale.txt')
+
+    def _save(self, fn):
+        fp = open(fn, 'w')
+        _ts = {
+            'TS': self.TS,
+            'year': self.year,
+            'month': self.month,
+            'day': self.day,
+        }
+        json.dump(_ts, fp)
+        fp.close()
+
+    def _load(self, fn):
+        try:
+            fp = open(fn, 'r')
+            _ts = json.load(fp)
+            self.TS = _ts['TS']
+            self.year = _ts['year']
+            self.month = _ts['month']
+            self.day = _ts['day']
+            fp.close()
+        except:
+            self.TS = 0
+            self.year = 0
+            self.month = 0
+            self.day = 0
+            self._save(fn)
+
+    def getTS(self):
+        return self.TS, self.year, self.month, self.day
+
+    def addTS(self):
+        self.TS += 1
+        if (self.TS % 24) == 0:
+            self.day += 1
+            if self.day == 30:
+                self.day = 0
+                self.month += 1
+                if self.month == 12:
+                    self.month = 0
+                    self.year += 1
+
+    def saveTS(self):
+        self._save('timescale.txt')
 
 class Screen:
 
     def __init__(self):
         self.stdscr = curses.initscr()
-        self._max_y, self._max_x = self.stdscr.getmaxyx()
-        self._max_x -= 3
-        self._max_y -= 3
+        self.MAX_Y, self.MAX_X = self.stdscr.getmaxyx()
+        self.window = self.stdscr.subwin(self.MAX_Y,self.MAX_X,0,0)
+        self.main_window = self.window.subwin(self.MAX_Y-2,self.MAX_X-MENU_W-3,1,1)
+        self.menu_window = self.window.subwin(self.MAX_Y-2,MENU_W,1,self.MAX_X-MENU_W-2)
+        self._max_y = self.MAX_Y-4
+        self._max_x = self.MAX_X-MENU_W-6
         self.screen = {}
+        self.TS = TimeScale()
         self.loadData('screen.txt')
 
     def loadData(self, file_name):
@@ -26,9 +84,9 @@ class Screen:
                 self.screen[int(_k)] = _screen[_k]
             fp.close()
         except:
-            for _x in range(0, self._max_x+1):
+            for _x in range(0, self._max_x):
                 self.screen[_x] = []
-                for _y in range(0, self._max_y+1):
+                for _y in range(0, self._max_y):
                     self.screen[_x].append([_y, ' ', 0, False])
 
     def saveData(self):
@@ -73,7 +131,7 @@ class Screen:
         self.display_info(chr, x, y, colorpair=colorpair)
 
     def debug(self, str):
-        self.stdscr.addstr(self._max_y-1, 2, str, curses.color_pair(3))
+        self.window.addstr(self._max_y-1, 2, str, curses.color_pair(3))
 
     def isEdge(self,x,y,edge):
 
@@ -101,25 +159,27 @@ class Screen:
         return False
 
     def refresh(self, force=False, edge='^'):
+
+        self.TS.addTS()
+
         for _x in self.screen:
             for _y in self.screen[_x]:
-                """
-                if force or _y[3]:
-                    self.stdscr.addstr(_y[0]+1, _x+1, _y[1], curses.color_pair(_y[2]) | curses.A_BOLD)
-                    self.screen[_x][_y[0]][3] = False
-                """
                 if self.isEdge(_x,_y[0],edge):
-                    self.stdscr.addstr(_y[0]+1, _x+1, edge, curses.color_pair(_y[2]) | curses.A_BOLD)
+                    self.main_window.addstr(_y[0]+1, _x+1, edge, curses.color_pair(_y[2]) | curses.A_BOLD)
                 elif force or _y[3]:
-                    self.stdscr.addstr(_y[0]+1, _x+1, _y[1], curses.color_pair(_y[2]) | curses.A_BOLD)
+                    self.main_window.addstr(_y[0]+1, _x+1, _y[1], curses.color_pair(_y[2]) | curses.A_BOLD)
                     self.screen[_x][_y[0]][3] = False
-        self.stdscr.refresh()
+        _t, _year, _month, _day = self.TS.getTS()
+        self.menu_window.addstr(1, 2, "TimeScale: % 8d" % _t)
+        self.menu_window.addstr(2, 2, "Date: % 6d-%02d-%02d" % (_year, _month+1, _day+1))
+        self.main_window.refresh()
+        self.menu_window.refresh()
 
     def get_ch_and_continue(self, wait):
         if wait:
-            self.stdscr.nodelay(0)
-        ch = self.stdscr.getch()
-        self.stdscr.nodelay(1)
+            self.window.nodelay(0)
+        self.window.nodelay(1)
+        ch = self.window.getch()
         return ch
 
     def set_win(self):
@@ -135,9 +195,12 @@ class Screen:
         curses.noecho()
         curses.cbreak()
         self.stdscr.nodelay(1)
-        self.stdscr.box()
+        self.window.box()
+        self.main_window.box()
+        self.menu_window.box()
 
     def unset_win(self):
+        self.TS.saveTS()
         curses.nocbreak()
         self.stdscr.keypad(0)
         curses.echo()
