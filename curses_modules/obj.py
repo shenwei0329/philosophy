2d5a3f98732d181f8f9bc4ecd95956ff6e53a9eb
沈伟 <shenwei@chenweideMac-mini.local>
2018-01-27 23:31:08 +0800

    在群体中进一步完善“个体”特性。

--- curses_modules/obj.py
+++ curses_modules/obj.py
@@ -2,31 +2,32 @@
 #
 #   对象类 Obj
 #   ==========
+#   社区群体实体
+#
 #
 
 from policy import Policy
-import json
-import resource
+import resource, basic
+import uuid
 
 
 class Obj:
+    """
+    群体：
+    """
 
     def __init__(self, name, init_x, init_y, ch, color):
         self.name = name
+        _fn = "dot%s.txt" % self.name
+        self.backup = basic.BackUp(_fn)
         self.chr = ch
         self.color = color
         self.V = (0., 0., 0., 0.,)
         self.P = []
-        """初值：每个群体100人
-        """
-        for _i in range(100):
-            self.P.append(resource.Ps())
         self.policy = Policy()
-        try:
-            self.load()
-        except:
-            self.X = init_x + self.policy._func()
-            self.Y = init_y + self.policy._func()
+        self.X = init_x + self.policy._func()
+        self.Y = init_y + self.policy._func()
+        self.load()
 
     def move(self):
         self.V = self.policy.getPolicy()
@@ -37,18 +38,35 @@ class Obj:
         return int(self.X), int(self.Y), self.chr, self.color
 
     def save(self):
-        fp = open('dot%s.txt' % self.name, 'w')
-        json.dump({"x":self.X, "y":self.Y, "ch":self.chr, "c":self.color}, fp)
-        fp.close()
+        _data = {
+            "x": self.X,
+            "y": self.Y,
+            "ch": self.chr,
+            "c": self.color,
+            "persons": []
+        }
+        for _p in self.P:
+            _data['persons'].append(_p.show_name())
+            _p.save()
+        self.backup.save(_data)
 
     def load(self):
-        fp = open('dot%s.txt' % self.name, 'r')
-        _info = json.load(fp)
-        self.X = _info["x"]
-        self.Y = _info["y"]
-        self.chr = _info["ch"]
-        self.color = _info["c"]
-        fp.close()
+        _info = self.backup.load()
+        if _info is not None:
+            self.X = _info["x"]
+            self.Y = _info["y"]
+            self.chr = _info["ch"]
+            self.color = _info["c"]
+            for _name in _info["persons"]:
+                self.P.append(resource.Ps(_name))
+        else:
+            """初值：每个群体100人
+            """
+            for _i in range(100):
+                """个体姓名
+                """
+                _name = "%s-%s" % (self.name, uuid.uuid4())
+                self.P.append(resource.Ps(_name))
 
     def time_scale(self, ts):
         _alive = 0
